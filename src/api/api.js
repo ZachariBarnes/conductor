4cb890d2ae762fc5323bc4df604e20f485de94da
Zachari <Zachari.Barnes@daugherty.com>
2022-02-21 10:15:49 -0600

    fetch from json?

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,15 @@
 import axios from "axios";
 
 export function getData() {
+  try{
   return axios.get("/data/content.yml");
+  }
+  catch (error) {
+    console.log(error);
+    console.log("trying to load from JSON");
+    fetch("/data/content.json")
+    .then(response => {
+   return response.json();
+})
+  }
 }
