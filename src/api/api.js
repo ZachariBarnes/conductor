e4729285c6d4a58294f56ad967a0de2f9472e765
Zachari <Zachari.Barnes@daugherty.com>
2022-02-21 10:25:31 -0600

    adding data

--- src/api/api.js
+++ src/api/api.js
@@ -1,14 +1,14 @@
-import axios from "axios";
+// import axios from "axios";
 const data = require('/data/content.json');
 console.log(data);
 
 export function getData() {
-  try{
-  return axios.get("/data/content.yml");
-  }
-  catch (error) {
-    console.log(error);
+  // try{
+  // return axios.get("/data/content.yml");
+  // }
+  // catch (error) {
+  //   console.log(error);
     console.log("trying to load from JSON");
     return data;
   }
-}
+// }
